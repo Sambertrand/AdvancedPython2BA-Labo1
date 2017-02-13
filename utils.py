14a06c4a39fec42a04ed2a44ee746efea2638a9f
Sam BERTRAND <15027@ecam.be>
2017-02-13 15:44:33 +0100

    1ere modif

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
+import cmath
 
 def fact(n):
     """Computes the factorial of a natural number.
@@ -9,6 +10,11 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
+    if n = 0: return 1
+    else:
+        for i in range(n):
+            tot *= i
+        return tot
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -17,7 +23,11 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    d = (b ** 2) - (4 * a * c)
+    sol1 = (-b - cmath.sqrt(d)) / (2 * a)
+    sol2 = (-b + cmath.sqrt(d)) / (2 * a)
+
+    print('The solutions are {0} and {1}'.format(sol1, sol2))
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -28,9 +38,9 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+    return 1
 
 if __name__ == '__main__':
     print(fact(5))
     print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate('x ** 2 - 1', -1, 1))0
