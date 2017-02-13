3aee866e117665dca6eb0f8bfe21fb030b9da2ab
Sam Bertarnd <15027@ecam.be>
2017-02-13 17:08:34 +0100

    Fix utils to pass tests

--- utils.py
+++ utils.py
@@ -1,46 +1,68 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
-import cmath
+import math
+
 
 def fact(n):
     """Computes the factorial of a natural number.
-    
+
     Pre: -
     Post: Returns the factorial of 'n'.
-    Throws: ValueError if n < 0
+    Throws: ValueError if n < 0.
     """
-    if n = 0: return 1
-    else:
-        for i in range(n):
-            tot *= i
-        return tot
+    R = math.factorial(n)
+    return R
+
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
-    
+
     Pre: -
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    d = (b ** 2) - (4 * a * c)
-    sol1 = (-b - cmath.sqrt(d)) / (2 * a)
-    sol2 = (-b + cmath.sqrt(d)) / (2 * a)
+    if a == 0 and b == 0:
+        return None
 
-    print('The solutions are {0} and {1}'.format(sol1, sol2))
+    if a == 0:
+        RS = -c / b
+        return (RS)
+
+    D = b ** 2 - 4 * a * c
+    if D < 0:
+        return None
+    elif D == 0:
+        R0 = (-b) / (2 * a)
+        return R0
+    else:
+        R1 = (-b - math.sqrt(D)) / (2 * a)
+        R2 = (-b + math.sqrt(D)) / (2 * a)
+        return (R1, R2)
+
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
-    
+
     Pre: 'function' is a valid Python expression with x as a variable,
          'lower' <= 'upper',
          'function' continuous and integrable between 'lower‘ and 'upper'.
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    return 1
+    steps = 5000
+    h = (upper - lower) / steps
+    x = lower
+
+    integral = 0
+    while x < upper:
+        integral += eval(function) * h
+        x += h
+    return integral
+
 
 if __name__ == '__main__':
     print(fact(5))
-    print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))0
+    print(roots(0, -4, 20))
+    print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate('x', 0, 4))
